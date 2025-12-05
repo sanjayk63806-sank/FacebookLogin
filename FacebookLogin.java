b03ff7fa1d75d26652ddcc614ceb8ede9be515f2
sanjayk63806-sank <sanjayk63806@gmail.com>
2025-12-05 15:02:33 +0530

    Add files via upload
    day 4 Facebook

---
+++ FacebookLogin.java
@@ -0,0 +1,12 @@
+package learnautomation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FacebookLogin {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+
+	}
+
+}
